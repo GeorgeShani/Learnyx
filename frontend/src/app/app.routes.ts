0a031195802dbb723d16ad48b0deeeffd42b6fbf
GeorgeShani <shanidze.geo2004@gmail.com>
2025-09-09 13:08:26 +0400

    Add new dashboard route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,16 +3,36 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./layout/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
-    loadChildren: () => import("./features/main/main.routes").then(m => m.routes)
+    loadComponent: () =>
+      import('./layout/main-layout/main-layout.component').then(
+        (m) => m.MainLayoutComponent
+      ),
+    loadChildren: () =>
+      import('./features/main/main.routes').then((m) => m.routes),
   },
   {
     path: 'auth',
-    loadComponent: () => import("./layout/auth-layout/auth-layout.component").then(m => m.AuthLayoutComponent),
-    loadChildren: () => import("./features/auth/auth.routes").then(m => m.routes)
+    loadComponent: () =>
+      import('./layout/auth-layout/auth-layout.component').then(
+        (m) => m.AuthLayoutComponent
+      ),
+    loadChildren: () =>
+      import('./features/auth/auth.routes').then((m) => m.routes),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./layout/dashboard-layout/dashboard-layout.component').then(
+        (m) => m.DashboardLayoutComponent
+      ),
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.routes').then((m) => m.routes),
   },
   {
     path: '**',
-    loadComponent: () => import("./layout/error-layout/error-layout.component").then(m => m.ErrorLayoutComponent)
-  }
+    loadComponent: () =>
+      import('./layout/error-layout/error-layout.component').then(
+        (m) => m.ErrorLayoutComponent
+      ),
+  },
 ];
