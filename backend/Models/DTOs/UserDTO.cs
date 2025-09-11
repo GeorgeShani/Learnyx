42490dc214b50e11214b87ea179539481215c3e5
GeorgeShani <shanidze.geo2004@gmail.com>
2025-09-11 20:50:52 +0400

    Add Login, Register Request models and User DTO

---
+++ backend/Models/DTOs/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace learnyx.Models.DTOs;
+
+public class UserDTO
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+    public string? Avatar { get; set; }
+    public string AuthProvider { get; set; } = string.Empty;
+}
---
+++ backend/Utilities/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using learnyx.Models.DTOs;
+using learnyx.Models.Entities;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace learnyx.Utilities.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<RegisterRequest, User>();
+        CreateMap<User, UserDTO>();
+    }
+}
