90c511647269e0c384c261aed4c8de6df8b5883f
GeorgeShani <shanidze.geo2004@gmail.com>
2025-09-12 14:42:03 +0400

    Modify Auth Response

--- backend/Models/Responses/AuthResponse.cs
+++ backend/Models/Responses/AuthResponse.cs
@@ -5,6 +5,5 @@ namespace learnyx.Models.Responses;
 public class AuthResponse
 {
     public string Token { get; set; } = string.Empty;
-    public DateTime ExpiresAt { get; set; }
     public UserDTO User { get; set; } = null!;
 }
