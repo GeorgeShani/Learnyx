f1f2cf1c759e7a60aba8bc7fa7af834084f3e4f6
GeorgeShani <shanidze.geo2004@gmail.com>
2025-09-12 14:41:12 +0400

    Add CORS Config

---
+++ backend/Configuration/CorsConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace learnyx.Configuration;
+
+public static class CorsConfiguration
+{
+    public static IServiceCollection AddCorsConfiguration(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", policy =>
+            {
+                policy.AllowAnyOrigin()  // Allow all domains
+                    .AllowAnyMethod()    // Allow all HTTP methods (GET, POST, PUT, DELETE...)
+                    .AllowAnyHeader();   // Allow all headers
+            });
+        });
+        
+        return services;   
+    }   
+}
