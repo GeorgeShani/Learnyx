cb59233cd9b5a97d6ec2d9259116aad7a09bed0d
GeorgeShani <shanidze.geo2004@gmail.com>
2025-09-12 14:42:26 +0400

    Add all services into one method

---
+++ backend/Configuration/ServicesConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using learnyx.Utilities.Mappings;
+using learnyx.Repositories.Interfaces;
+using learnyx.Repositories.Implementation;
+using learnyx.SMTP.Implementation;
+using learnyx.SMTP.Interfaces;
+using learnyx.Utilities.Constants;
+
+namespace learnyx.Configuration;
+
+public static class ServicesConfiguration
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IEmailService, EmailService>();
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+        // FluentValidation
+        services.AddValidatorsFromAssemblyContaining<Program>();
+
+        // AutoMapper
+        services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfile>());
+
+        // SMTP settings
+        services.Configure<SmtpSettings>(configuration.GetSection("Smtp"));
+
+        return services;
+    }
+}
