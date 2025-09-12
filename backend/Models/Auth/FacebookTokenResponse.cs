3b9cfd5fbae98ed5e64aa2ef32b7efd5df783746
GeorgeShani <shanidze.geo2004@gmail.com>
2025-09-12 09:49:08 +0400

    Add Another Facebook Auth Model

---
+++ backend/Models/Auth/FacebookTokenResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace learnyx.Models.Auth;
+
+public class FacebookTokenResponse
+{
+    public string AccessToken { get; set; } = string.Empty;
+    public string TokenType { get; set; } = string.Empty;
+    public int ExpiresIn { get; set; }
+}
