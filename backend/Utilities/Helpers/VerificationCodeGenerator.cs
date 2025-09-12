3a8755dee44033e501c3443b9ac7a4530c751e11
GeorgeShani <shanidze.geo2004@gmail.com>
2025-09-12 19:40:55 +0400

    Add Verification Code Helper

---
+++ backend/Utilities/Helpers/VerificationCodeGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+
+namespace learnyx.Utilities.Helpers;
+
+public static class VerificationCodeGenerator
+{
+    public static string Generate6DigitCode()
+    {
+        // Generates a number between 0 and 999999, and pads with zeros if needed
+        return RandomNumberGenerator.GetInt32(0, 1000000).ToString("D6");
+    }
+}
